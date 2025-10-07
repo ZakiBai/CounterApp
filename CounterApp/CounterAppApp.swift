47d286abc5e0f10d75cb6279df7cff0c982a76f1
Zaki Menzhanov <menza.zm@gmail.com>
2025-10-07 23:11:57 +0500

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Zaki Menzhanov on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
