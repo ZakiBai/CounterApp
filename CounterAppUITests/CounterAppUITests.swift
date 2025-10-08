b9f321e649b4db971f0c654326f6cc0301b39c16
Zaki Menzhanov <menza.zm@gmail.com>
2025-10-08 18:33:15 +0500

    Linter added

--- CounterAppUITests/CounterAppUITests.swift
+++ CounterAppUITests/CounterAppUITests.swift
@@ -25,8 +25,8 @@ final class CounterAppUITests: XCTestCase {
     @MainActor
     func testExample() throws {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+//        let app = XCUIApplication()
+//        app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
