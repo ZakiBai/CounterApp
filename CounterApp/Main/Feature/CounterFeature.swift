af3d1f1e637d2508ebeae90ba25957c4b01b3911
Zaki Menzhanov <menza.zm@gmail.com>
2025-10-13 17:39:54 +0500

    Timer and tests added

--- CounterApp/Main/Feature/CounterFeature.swift
+++ CounterApp/Main/Feature/CounterFeature.swift
@@ -12,39 +12,70 @@ import ComposableArchitecture
 struct CounterFeature {
 
     @Dependency(\.catFact) var catFact
+    @Dependency(\.continuousClock) var clock
 
     @ObservableState
     struct State: Equatable {
         var count = 0
         var numberFact: String?
+        var isLoading: Bool = false
+        var isTimerRunning: Bool = false
     }
 
     enum Action: Equatable {
         case decrementButtonTapped
         case incrementButtonTapped
         case numberFactButtonTapped
-        case numberFactButtonResponse(String  )
+        case numberFactButtonResponse(String)
+        case toggleButtonTimerTapped
+        case timerTick
     }
+
+    enum CancelID { case timer }
 
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.numberFact = nil
                 return .none
 
             case .incrementButtonTapped:
                 state.count += 1
+                state.numberFact = nil
                 return .none
 
             case .numberFactButtonTapped:
+                state.isLoading = true
                 return .run { send in
                     let fact = try await catFact.fetch()
                     await send(.numberFactButtonResponse(fact))
                 }
 
             case let .numberFactButtonResponse(fact):
+                state.isLoading = false
                 state.numberFact = fact
+                return .none
+
+            case .toggleButtonTimerTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            for await _ in self.clock.timer(interval: .seconds(1)) {
+                                await send(.timerTick)
+                            }
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
+
+            case .timerTick:
+                state.count += 1
+                state.numberFact = nil
                 return .none
             }
         }
